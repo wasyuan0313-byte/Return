0de9b13b0a5766e798acab431b192cc4927a8d3a
wasyuan0313-byte <wasyuan0313@gmail.com>
2026-09-03 13:36:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
  *         每次「新增部署作業」都會換一組網址，換了就要回來改這裡。
  */
 window.DONREN_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbyTfauHZ4yrtzlU30tVxal5lMdS3ttaAiIS_61lg0y3jugVf88Iz5xA2KYDdWPZ8xFa/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbynlOCvjdP88ObiUz7hurr-Cz9sxwuEpUFjEbTBlps0iNUZxVTKCZq_2QJ20CnApYcx/exec',
 };
